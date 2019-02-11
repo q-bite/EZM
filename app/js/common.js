25f97ec6e323290854935bf53e5fcf872103db72
MAXON-BY <maxon.pro2015@gmail.com>
2019-02-11 15:12:13 +0300

    end of desktop version

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,30 @@
-$(function() {
+/* mobile NAV */
+$(document).ready(function() {
 
 	$('a[href="#nav"]').on('click', function(e){
         e.preventDefault();
         $('.contain-window').toggleClass('nav-active');
     })
 
+});
+
+/* Popup Magnific Gallery */
+$(document).ready(function() {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by EZMSIGN Company</small>';
+            }
+        }
+    });
 });
